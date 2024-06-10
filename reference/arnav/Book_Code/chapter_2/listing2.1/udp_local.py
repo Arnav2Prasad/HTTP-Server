6cb0defa2433ad66250c95dd1e98757d649ce193
Arnav Prasad <122086367+Arnav2Prasad@users.noreply.github.com>
2024-06-11 00:49:48 +0530

    Update udp_local.py

--- reference/arnav/Book_Code/chapter_2/listing2.1/udp_local.py
+++ reference/arnav/Book_Code/chapter_2/listing2.1/udp_local.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # Foundations of Python Network Programming - Chapter 2 - udp_local.py
 # UDP client and server on localhost
@@ -15,10 +16,10 @@ if sys.argv[1:] == ['server']:
     while True:
         data, address = s.recvfrom(MAX)
         print('The client at', address, 'says', repr(data))
-        s.sendto('Your data was %d bytes' % len(data), address)
+        s.sendto(b'Your data was %d bytes' % len(data), address)
 elif sys.argv[1:] == ['client']:
     print('Address before sending:', s.getsockname())
-    s.sendto('This is my message', ('127.0.0.1', PORT))
+    s.sendto(b'This is my message', ('127.0.0.1', PORT))
     print('Address after sending', s.getsockname())
     data, address = s.recvfrom(MAX)  # overly promiscuous - see text!
     print('The server', address, 'says', repr(data))
